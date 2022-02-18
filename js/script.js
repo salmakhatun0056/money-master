1f1733b5d1189ef21f8b58a90f90379f7eec6599
salmakhatun12345e <ummesalma0056@gmail.com>
2022-02-18 21:36:18 +0600

    error-complite

--- js/script.js
+++ js/script.js
@@ -67,74 +67,7 @@ document.getElementById('save-btn').addEventListener('click', function () {
     }
 })
 
-//     if (savingAmount.innerText > balanceAmount.innerText) {
-//         savingAmount.textContent = "You don't have enought money"
-//     }
-//     else {
-//         savingAmount.innerText = income * save / 100
 
-//     }
-
-
-
-
-
-
-
-// function getInputValue(product) {
-//     const productInput = document.getElementById(product + '-input')
-//     const productNumber = productInput.value
-//     const productAmount = parseInt(productNumber);
-//     return productAmount;
-// }
-
-// document.getElementById('calculate').addEventListener('click', function () {
-//     const income = getInputValue('income')
-//     const expenses = getInputValue('expenses')
-//     const rent = getInputValue('rent')
-//     const cloth = getInputValue('clothes')
-
-
-//     const totalAmount = document.getElementById('total-amount')
-
-//     if (isNaN(totalAmount.innerText)) {
-//         totalAmount.textContent = 'please enter valid number'
-
-//     }
-
-//     else if (totalAmount.innerText > 0) {
-//         totalAmount.textContent = 'please enter possitive number'
-
-//     }
-
-//     else {
-//         totalAmount.innerText = expenses + rent + cloth
-//     }
-
-//     const balanceAmount = document.getElementById('balance-amount')
-//     balanceAmount.innerText = income - totalAmount.innerText
-//     console.log(balanceAmount.innerText)
-// })
-
-// document.getElementById('save-btn').addEventListener('click', function () {
-//     const save = getInputValue('save')
-//     const savingAmount = document.getElementById('saving-amount')
-//     savingAmount.innerText = save;
-//     const income = getInputValue('income')
-
-//     savingAmount.innerText = income * save / 100
-
-
-//     const remainingAmount = document.getElementById('remainig-amount')
-//     const balanceAmount = document.getElementById('balance-amount')
-//     remainingAmount.innerText = balanceAmount.innerText - savingAmount.innerText
-
-
-//     if (savingAmount.innerText > balanceAmount.innerText) {
-//         savingAmount.textContent = "You don't have enought money you need"
-//     }
-
-// })
 
 
 
